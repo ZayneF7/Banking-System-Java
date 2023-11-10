472df5a2f2e369fca5a2a3fa5002fd2d6b85b514
Zayne Foster <zaynef7@gmail.com>
2023-11-10 13:42:51 -0500

    Includes implemented CommandStorage and its associated test cases

---
+++ src/main/java/CommandStorage.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class CommandStorage {
+	private ArrayList<String> commands;
+
+	public CommandStorage() {
+		commands = new ArrayList<String>();
+	}
+
+	public ArrayList<String> getCommands() {
+		return commands;
+	}
+
+	public void addCommand(String command) {
+		commands.add(command);
+	}
+}
---
+++ src/test/java/CommandStorageTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CommandStorageTest {
+	CommandStorage commandStorage;
+
+	@BeforeEach
+	public void setUp() {
+		commandStorage = new CommandStorage();
+	}
+
+	@Test
+	public void storage_is_created_with_no_commands() {
+		assertEquals(0, commandStorage.getCommands().size());
+	}
+
+	@Test
+	public void storage_has_one_command_after_one_command_is_added() {
+		commandStorage.addCommand("Foo");
+		assertEquals(1, commandStorage.getCommands().size());
+	}
+
+	@Test
+	public void storage_has_two_commands_after_two_commands_is_added() {
+		commandStorage.addCommand("Foo");
+		commandStorage.addCommand("Foobar");
+		assertEquals(2, commandStorage.getCommands().size());
+	}
+}
